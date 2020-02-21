5a32979c96eddcb3af2d04f972fd12f868a98934
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-02-21 09:34:57 -0600

    Add project files.

---
+++ VentaPieMVC/Migrations/20200220183135_seedData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VentaPieMVC.Migrations
+{
+    public partial class seedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[] { 1, "CAT1", null });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[] { 2, "CAT2", null });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[] { 3, "CAT3", null });
+
+            migrationBuilder.InsertData(
+                table: "Pies",
+                columns: new[] { "PieId", "AllegryInformation", "CategoryId", "ImageThumbnailUrl", "ImageUrl", "InStock", "IsPieOfTheWeek", "LongDescription", "Name", "Price", "ShortDescription" },
+                values: new object[] { 1, "", 1, "", "", true, true, "lonooonong", "Apple Pie", 0m, "short deds" });
+
+            migrationBuilder.InsertData(
+                table: "Pies",
+                columns: new[] { "PieId", "AllegryInformation", "CategoryId", "ImageThumbnailUrl", "ImageUrl", "InStock", "IsPieOfTheWeek", "LongDescription", "Name", "Price", "ShortDescription" },
+                values: new object[] { 2, "", 2, "", "", true, false, "lonooonong", "Cheese Pie", 0m, "short deds" });
+
+            migrationBuilder.InsertData(
+                table: "Pies",
+                columns: new[] { "PieId", "AllegryInformation", "CategoryId", "ImageThumbnailUrl", "ImageUrl", "InStock", "IsPieOfTheWeek", "LongDescription", "Name", "Price", "ShortDescription" },
+                values: new object[] { 3, "", 3, "", "", true, false, "lonooonong", "stuff pie", 0m, "short deds" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+        }
+    }
+}
